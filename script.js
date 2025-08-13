d34d621d8f399b3cdc0eab64ec9d36bbfbcd21e7
Railson de Oliveira Silva <railson.de.oliveira.silva.ros@gmail.com>
2025-08-13 20:19:10 -0300

    script pronto e comentado

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 //Estado da aplicação
-const tarefas = ["Estudar html", "Estudar CSS", "Estudar JS"]
+const tarefas = ["Estudar html", "Estudar CSS", "Estudar JS"] //armazena as tarefas incias em uma array
 
-function render() {
-    
-
+//alteradores da aplicação
+function add() { //função que irá adicionar a nova tarefa
+    const input = document.querySelector("input") //Cria uma const chamada "input" que armazena o input
+    const nova_tarefa = input.value //transfere o valor do input para a const "nova_tarefa"
+    tarefas.push(nova_tarefa) //adiciona a nova tarefa a array chamada "tarefas"
+    input.value = ""    //limpa o campo "input" após finalizar a função
+    render() //renderiza novamente as tarefas pré existentes
 }
 
+//renderização
+function render() { //função para renderizar as tarefas inciais 
+    const ul = document.querySelector("ul") //cria uma const "ul" que armazena a ul
+    ul.innerHTML = "" //limpa o valor da ul
+    tarefas.forEach(function(tarefa){
+        const li = document.createElement("li")
+    li.innerText = tarefa
+    ul.appendChild(li)                  
+    })          
+    }
+    
+ //iniciar
+render()
